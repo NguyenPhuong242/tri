f3b5569219a32d239e8b2da89a8ef1c450735b81
NguyenPhuong <Nguyenphuongvn24220@gmail.com>
2023-02-05 00:14:30 +0100

    prof

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
+import java.util.LinkedList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         
-        Suite a = new Suite(300);
-        // System.out.println(a.clone().toString());
-        // Tribull.tri_bulle(a);
-        // trifusion.triFusion(a);
-        // rapide.quicksort(a, 0, a.size()-1);
-        // tritas.heapSort(a);
+        // Suite a = new Suite(300);
+        // // System.out.println(a.clone().toString());
+        // // Tribull.tri_bulle(a);
+        // // trifusion.triFusion(a);
+        // // rapide.quicksort(a, 0, a.size()-1);
+        // // tritas.heapSort(a);
 
-        System.out.println(a.toString());
+        // System.out.println(a.toString());
+
+        LinkedList<Integer> test = new LinkedList<>();
+        Random random = new Random();
+        for(int i = 0; i < 10; i++){
+            test.add(random.nextInt(10));
+        }
+        trifusion.trifusion(test);
+        System.out.println(test.toString());
+        System.out.println(test.size());
 
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
--- trifusion.java
+++ trifusion.java
@@ -1,43 +1,52 @@
-public class trifusion{
-    public static void triFusion(Suite suite){
-		if (suite.size()>0){
-			triFusion(suite,0,suite.size()-1);
+import java.util.LinkedList;
+
+public class trifusion {
+
+	public static LinkedList<Integer> trifusion(LinkedList<Integer> list) {
+		if (list.size() <= 1) {
+			return list;
 		}
+		
+		LinkedList<Integer> gauche = new LinkedList<Integer>();
+		LinkedList<Integer> droite = new LinkedList<Integer>();
+
+		for (int i = 0; i < list.size()/2; i++) {
+			gauche.add(list.remove());
+			
+		}
+		for (int i = list.size()/2; i < list.size(); i++) {
+			droite.add(list.remove());
+		}
+		return fusioner(trifusion(gauche), trifusion(droite));
+
 	}
 
-	private static void triFusion(Suite suite,int debut,int fin){
-		if (debut!=fin){
-			int milieu=(fin+debut)/2;
-			triFusion(suite,debut,milieu);
-			triFusion(suite,milieu+1,fin);
-			fusion(suite,debut,milieu,fin);
+	public static LinkedList<Integer> fusioner(LinkedList<Integer> gauche, LinkedList<Integer> droite) {
+		LinkedList<Integer> total = new LinkedList<Integer>();
+
+		while (!gauche.isEmpty() && !droite.isEmpty()) {
+			int g1 = gauche.remove();
+			int d1 = droite.remove();
+			if (d1 < g1) {
+				total.add(d1);
+				gauche.addFirst(g1);
+			} 
+			else {
+				total.add(g1);
+				droite.addFirst(d1);
+			}
 		}
+		while (!gauche.isEmpty()) {
+			total.add(gauche.remove());
+		}
+		while (!droite.isEmpty()) {
+			total.add(droite.remove());
+		}
+		return total;
 	}
 
-	private static void fusion(Suite suite,int debut1,int fin1,int fin2){
-		int debut2=fin1+1;
-		Suite table1=new Suite(fin1-debut1+1);
-		for(int i=debut1;i<=fin1;i++){
-			table1.set(i-debut1, suite.get(i));
-		}
-		int compteur1=debut1;
-		int compteur2=debut2;
-		for(int i=debut1;i<=fin2;i++){        
-			if (compteur1==debut2){
-				break;
-			}
-			else if (compteur2==(fin2+1)){
-				suite.set(i, table1.get(compteur1-debut1));
-				compteur1++;
-			}
-			else if (table1.get(compteur1-debut1)<suite.get(compteur2)){
-				suite.set(i, table1.get(compteur1-debut1));
-				compteur1++;
-			}
-			else{
-				suite.set(i, suite.get(compteur2));
-				compteur2++;
-			}
-		}
+	public String toString(LinkedList<Integer> list){
+		return list.toString();
 	}
+
 }
